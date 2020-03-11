f876cbd491c6357bfd290732329170715ee7cb17
PedroHenriqueDevBR <p.lyma18@gmail.com>
2020-03-10 21:51:23 -0300

    feat: implement querys on controller

--- lib/models/Client.dart
+++ lib/models/Client.dart
@@ -1,34 +1,10 @@
 class Client {
   int id;
-  String _name;
-  String _address;
-  String _phone;
-  String _mail;
+  String name;
+  String phone;
+  String email;
+  String address;
+  String observations;
 
-  Client(this._name, this._address, this._phone, this._mail);
-
-  String get mail => _mail;
-
-  set mail(String value) {
-    _mail = value;
-  }
-
-  String get phone => _phone;
-
-  set phone(String value) {
-    _phone = value;
-  }
-
-  String get address => _address;
-
-  set address(String value) {
-    _address = value;
-  }
-
-  String get name => _name;
-
-  set name(String value) {
-    _name = value;
-  }
-
+  Client({this.name, this.phone, this.email, this.address, this.observations});
 }
